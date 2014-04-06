f73b2dd9ddaa62a8043307c393ab8fc5ed108833
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2014-04-05 20:49:00 -0400

    Updates random.py example

--- examples/random.py
+++ examples/random.py
@@ -2,19 +2,35 @@
 #located in http://archive.ics.uci.edu/ml/datasets/Iris
 #and will attempt to query using one image against the whole dataset
 
-import numpy
-import falcon
+from sys import exit
+
+try:
+	import numpy
+except:
+	exit("Unable to import numpy. Exiting example.")
+
+try:
+	import falcon
+except:
+	exit("Unable to import falcon. Exiting example.")
+
+from time import time
+from math import floor 
 
 number_of_synthetic_features = 100
 number_of_synthetic_vectors_in_dataset = 100
 
+t = time()
 print 'Generating random query image'
-print 'Query image name: foo'
-query_image = [ 'foo', 1 ]
+print 'Query image name: img'
+query_image = [ 'img', 1 ]
 synthetic_feature_vector = numpy.random.rand(1,number_of_synthetic_features).tolist()[0]
 query_image.append( synthetic_feature_vector )
 query_set = [ query_image ]
+t = time() - t
+print "Elapsed time: " + str(t) + " seconds\n"
 
+t = time()
 print "Generating random dataset"
 dataset = []
 dataset.append( query_image )
@@ -24,8 +40,36 @@ for i in range(number_of_synthetic_vectors_in_dataset):
 	datum.append( synthetic_feature_vector )
 	dataset.append( datum )
 
-[iids, scores] = falcon.search.query( query_set, dataset, 
-	normalization='standard', debug=True )
+t = time() - t
+print "Elapsed time: " + str(t) + " seconds\n"
 
-for iid, score in zip(iids,scores):
-	print str(iid) + ":" + str(score)
+t = time()
+print "Querying with one images"
+[iids, scores] = falcon.search.query( query_set, dataset, normalization='standard', debug=True )
+t = time() - t
+print "Elapsed time: " + str(t) + " seconds\n"
+
+#icaoberg: you can ignore the next part of the code. i was just trying to prettify the results
+#there is a probably a quicker, leaner, cleaner way
+print "Top Ten Results!"
+
+#icaoberg: i will only display the top ten results
+iids = iids[0:9]
+scores = scores[0:9]
+
+try:
+	from tabulate import tabulate
+	rank = 0
+	table = []
+	for index in range(len(iids)):
+		table.append([str(rank), str(iids[index]), str(scores[index])])
+		rank = rank + 1
+
+	print tabulate(table, headers=["Ranking","Identifier", "Score"])
+except:
+	print "rank\tiid\t\tscore"
+
+	rank = 0
+	for iid, score in zip(iids,scores):
+		print str(rank) + "\t" + str(iid) + "\t\t" + str(score)
+		rank = rank + 1
