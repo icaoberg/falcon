1b96da58f1e1d75ce6c72e5889363dff15bec3f1
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-01-30 22:54:08 -0500

    Cleaned up examples

--- examples/metrics.py
+++ examples/metrics.py
@@ -1,11 +1,11 @@
-import urllib
-from numpy import genfromtxt
-from os import remove
+import urllib.request
+import numpy as np
+import os
 import halcon
-from time import time
-from sys import exit
+from tabulate import tabulate
 
-print '''
+print(
+    """
 This example uses the wine dataset from
 
 Machine Learning Repository
@@ -13,40 +13,35 @@ Center for Machine Learning and Intelligent Systems
 http://archive.ics.uci.edu/ml/datasets/Wine
 
 This example uses this dataset to compare the different metrics available in FALCON
-'''
+"""
+)
 
-url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
-filename = 'data.csv'
-urllib.urlretrieve( url, filename )
-data = genfromtxt( filename, delimiter=',' )
-query_wines = []
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+filename = "data.csv"
+urllib.request.urlretrieve(url, filename)
+data = np.genfromtxt(filename, delimiter=",")
 
+query_wines = [["wine0", 1, data[0]]]
 
-query_wines.append( ['wine0', 1, data[0]] )
+# metrics
+metrics = ["euclidean", "cityblock", "hamming"]
 
+os.remove(filename)
 
-#metrics
-metrics = ['euclidean','cityblock','hamming']
+dataset = [
+    ["wine" + str(counter), 1, datum] for counter, datum in enumerate(data, start=1)
+]
 
-remove(filename)
+data = {
+    metric: halcon.search.query(
+        query_wines, dataset, metric=metric, normalization="standard", debug=True
+    )[0]
+    for metric in metrics
+}
 
-dataset = []
-counter = 1
-for datum in data:
-	dataset.append([ 'wine' + str(counter), 1, datum ])
-	counter = counter + 1
+table = [
+    [index, data["euclidean"][index], data["cityblock"][index], data["hamming"][index]]
+    for index in range(20)
+]
 
-data = {}
-for metric in metrics:
-	[iids, scores] = halcon.search.query( query_wines, dataset, metric=metric, normalization='standard', debug=True )
-	data[metric] = iids
-
-#icaoberg: just in case people do not have the tabulate package
-from tabulate import tabulate
-table = []
-for index in range(20):
-	table.append([index, data['euclidean'][index], data['cityblock'][index], data['hamming'][index]])
-
-print tabulate(table, headers=["Ranking","Euclidean", "City Block", "Hamming"])
-exit(0)
-
+print(tabulate(table, headers=["Ranking", "Euclidean", "City Block", "Hamming"]))
--- examples/wine.py
+++ examples/wine.py
@@ -6,69 +6,76 @@ from halcon import search
 from time import time
 from sys import exit
 
-print('''
+print(
+    """
 This example uses the wine dataset from
 
 Machine Learning Repository
 Center for Machine Learning and Intelligent Systems
 http://archive.ics.uci.edu/ml/datasets/Wine
-''')
+"""
+)
 
-url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
-filename = 'data.csv'
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+filename = "data.csv"
 
 http = urllib3.PoolManager()
-with open(filename, 'wb') as out:
-    r = http.request('GET', url, preload_content=False)
+with open(filename, "wb") as out:
+    r = http.request("GET", url, preload_content=False)
     shutil.copyfileobj(r, out)
 
-data = genfromtxt( filename, delimiter=',' )
+data = genfromtxt(filename, delimiter=",")
 
 print("I will use the first three feature vectors as my query wine set")
 query_wines = []
-query_wines.append(['wine0', 1, data[0]])
-query_wines.append(['wine1', 1, data[1]])
-query_wines.append(['wine2', 1, data[2]])
+query_wines.append(["wine0", 1, data[0]])
+query_wines.append(["wine1", 1, data[1]])
+query_wines.append(["wine2", 1, data[2]])
 
-print("\nAnd I will use the rest of the feature vectors to find the most similar images")
+print(
+    "\nAnd I will use the rest of the feature vectors to find the most similar images"
+)
 dataset = []
 counter = 1
 for datum in data:
-	dataset.append([ 'wine' + str(counter), 1, datum ])
-	counter = counter + 1
+    dataset.append(["wine" + str(counter), 1, datum])
+    counter = counter + 1
 
 t = time()
-[iids, scores] = search.query( query_wines, dataset, metric='cityblock', normalization='standard' )
+[iids, scores] = search.query(
+    query_wines, dataset, metric="cityblock", normalization="standard"
+)
 t = time() - t
 print("Elapsed time: " + str(t) + " seconds\n")
 
-#icaoberg: i will only display the top ten results
+# icaoberg: i will only display the top ten results
 iids = iids[0:10]
 scores = scores[0:10]
 
-#icaoberg: just in case people do not have the tabulate package
+# icaoberg: just in case people do not have the tabulate package
 try:
-	from tabulate import tabulate
-	rank = 0
-	table = []
+    from tabulate import tabulate
 
-	for index in range(len(iids)):
-		table.append([str(rank), str(iids[index]), str(scores[index])])
-		rank = rank + 1
+    rank = 0
+    table = []
 
-	print(tabulate(table, headers=["Ranking","Identifier", "Score"]))
+    for index in range(len(iids)):
+        table.append([str(rank), str(iids[index]), str(scores[index])])
+        rank = rank + 1
+
+    print(tabulate(table, headers=["Ranking", "Identifier", "Score"]))
 except:
-	print("rank\tiid\t\tscore")
+    print("rank\tiid\t\tscore")
 
-	rank = 0
-	for iid, score in zip(iids,scores):
-		print(str(rank) + "\t" + str(iid) + "\t\t" + str(score))
-		rank = rank + 1
+    rank = 0
+    for iid, score in zip(iids, scores):
+        print(str(rank) + "\t" + str(iid) + "\t\t" + str(score))
+        rank = rank + 1
 
 remove(filename)
 
-#icaoberg: this line is neccesary so i can test this example in travis/jenkins
-if iids[0] == 'wine1':
- 	exit(0)
+# icaoberg: this line is neccesary so i can test this example in travis/jenkins
+if iids[0] == "wine1":
+    exit(0)
 else:
-	exit("\nTop match was not query image. Something went wrong with this example")
+    exit("\nTop match was not query image. Something went wrong with this example")
